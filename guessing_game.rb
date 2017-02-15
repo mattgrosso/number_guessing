a1f53f99983a74aceb43a1a7c650bb0e6b249f72
mattgrosso <mattgrosso@gmail.com>
2017-02-14 21:04:55 -0500

    Implemented judgemental code.

--- guessing_game.rb
+++ guessing_game.rb
@@ -11,28 +11,38 @@ def already_guessed (guess, guesses)
     guesses << guess
     return ""
   end
-end                                   # => :already_guessed
+end                              # => :already_guessed
+
+def bad_guess (guess, low, high)
+  if guess > high
+    puts "Are you even listening to the clues? That's way too high."
+  elsif guess < low
+    puts "Why do I even try with you? You know that's too low. Put in some effort."
+  end
+end
 
 def compare_to_random (guess, random)
-  if guess.to_i == random
-    puts "You win!"
+  if guess == random
     return "win"
-  elsif guess.to_i < random
-    puts "Too low"
-  elsif guess.to_i > random
-    puts "Too high"
+  elsif guess < random
+    return "low"
+  elsif guess > random
+    return "high"
   end
 end                                    # => :compare_to_random
 
 random = random_number  # => 50
 number_of_guesses = 1   # => 1
 guesses = []            # => []
+lowest_possible = 1
+highest_possible = 100
 
 loop do
-  input = gets.chomp  # ~> NoMethodError: undefined method `chomp' for nil:NilClass
+  input = gets.chomp.to_i  # ~> NoMethodError: undefined method `chomp' for nil:NilClass
 
   if number_of_guesses > 4
     puts "You lose"
+    puts "The number was #{random}"
     break
   end
 
@@ -40,7 +50,20 @@ loop do
     puts already_guessed(input, guesses)
   end
 
-  if compare_to_random(input, random) == "win"
+  bad_guess(input, lowest_possible, highest_possible)
+
+  if compare_to_random(input, random) == "low"
+    puts "Too low"
+    if input > lowest_possible
+      lowest_possible = input
+    end
+  elsif compare_to_random(input, random) == "high"
+    puts "Too high"
+    if input < highest_possible
+      highest_possible = input
+    end
+  elsif compare_to_random(input, random) == "win"
+    puts "You win!"
     break
   end
 
